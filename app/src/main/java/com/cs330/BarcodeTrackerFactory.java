b9c2898609a4c5e14377646910427b7850f1412d
MicicFilip <micic.filip@gmail.com>
2017-05-28 19:40:36 +0200

    i think that is it

---
+++ app/src/main/java/com/cs330/BarcodeTrackerFactory.java
@@ -0,0 +1,26 @@
+package com.cs330;
+
+import com.cs330.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Created by Filip on 28-May-17.
+ */
+
+class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+
+    private GraphicOverlay<BarcodeGraphic> mGraphicOverlay;
+
+    BarcodeTrackerFactory(GraphicOverlay<BarcodeGraphic> barcodeGraphicOverlay) {
+        mGraphicOverlay = barcodeGraphicOverlay;
+    }
+
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        BarcodeGraphic graphic = new BarcodeGraphic(mGraphicOverlay);
+        return new BarcodeGraphicTracker(mGraphicOverlay, graphic);
+    }
+}
